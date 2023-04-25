2e1f89460e1528d7cc1998e1f45fa11492017db8
WellingtonWoS <106634213+WellingtonWoS@users.noreply.github.com>
2023-04-24 23:33:57 -0300

    Create Ordenações.h

---
+++ Ordenações.h
@@ -0,0 +1,29 @@
+//1Bubblesort
+int bubblesort(v, tam);
+//2insertionsort
+void insertion(v,tam);
+//3selectionsort
+void swap(int xp, int intyp);
+void selectionSort(int arr[], int n);
+//4shellsort
+void shellsort(v,tam);
+//5Mergesort
+void mergesort(v, tam);
+void sort(int *v, int *c, int i, int f);
+void merge(int *v, int *c, int i, int m, int f);
+//6Quicksort
+void quick_sort(int *a, int left, int right);
+//7Heapsort
+void criaHeap (int vet, int i, int f);
+int heapsort(int *v,int tam);
+//8radixsort LSD
+int pega_Valor_Max(int array[], int n);
+void contador(int array[], int tamanho, int divisor);
+void radixsortLSD (v, tam);
+//9timsort
+void insertionSort(int arr[], int l, int r);
+void merge_timsort(int arr[], int temp[], int l, int m, int r);
+int min(int arg1, int arg2);
+void timSort(v, tam);
+//10 countingsort
+int countingsort(int *v, int tam);
